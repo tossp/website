4843c2229c2fe2fa533f08cc7f97c73905b45a92
砼砼 <code@tossp.com>
2022-03-20 01:25:00 +0800

    fix(comments): 修复定位不统一

--- assets/js/run.js
+++ assets/js/run.js
@@ -2,9 +2,10 @@
 window.addEventListener('load', () => {
   var articleElem = document.getElementsByTagName('article');
   if (articleElem.length && articleElem[0].className === 'page single') {
+    var pathID=window.location.pathname.replace(/\/$/,'')||'i.TossP'
     var metaElem = document.getElementsByClassName('post-meta-line');
     if (metaElem.length === 2) {
-      var visitor = "<i  class=\"far fa-eye fa-fw\"></i> &nbsp;阅读量 <span id=\"" + window.location.pathname + "\" class=\"waline-visitor-count\" >1</span> 次&nbsp;"
+      var visitor = "<i  class=\"far fa-eye fa-fw\"></i> &nbsp;阅读量 <span id=\"" + pathID + "\" class=\"waline-visitor-count\" >1</span> 次&nbsp;"
       metaElem[1].innerHTML=metaElem[1].innerHTML + visitor
     } else {
       console.warn('页面结构变更，定位meta失败', metaElem)
@@ -21,7 +22,7 @@ window.addEventListener('load', () => {
       requiredMeta: ['nick', 'mail'],
       copyright: false,
       dark: 'body[theme="dark"]',
-      path:window.location.pathname.replace(/\/$/,''),
+      path:pathID,
       emoji: [
         'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/weibo',
         'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili',
